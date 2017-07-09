9a2032cf32fee7c3ef1e1f737efcaab3d4e36a52
gabigb39 <gabigb39@yahoo.com>
2017-07-09 13:48:22 +0300

    Add files via upload

---
+++ DayNightCycle.cs
@@ -0,0 +1,33 @@
+﻿
+using UnityEngine;
+
+public class DayNightCycle : MonoBehaviour {
+
+    public float Speed;
+    public int Day = 1;
+    public bool newDay = false;
+    public int currentTime;
+	
+	void Update ()
+    {
+
+        transform.Rotate(Vector3.right * Time.deltaTime * Speed);
+
+        float sunX = transform.rotation.eulerAngles.x;
+
+        if (sunX >= 195)
+        {
+            transform.Rotate(-transform.rotation.eulerAngles);
+            NewDay();
+        }
+
+       currentTime = Mathf.RoundToInt(sunX);
+    }
+
+
+
+    void NewDay ()
+    {
+        Day++;
+    }
+}
